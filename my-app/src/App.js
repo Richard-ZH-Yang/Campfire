7d9db17c8c25b0e5067d3be4f8f2dc19fecac54d
Danny <dengzecheng@hotmail.com>
2021-11-14 17:26:01 -0800

    clean main page finish the router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,20 @@
 // import { BrowserRouter as Routes, Route } from "react-router-dom";
-import { BrowserRouter as Routes ,Route } from 'react-router-dom';
 import './App.css';
 
 import MainPage from "./containers/MainPage";
 import AddTagPage from "./containers/addTagPage";
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      {/* <Header /> */}
-{/*   
-        <Routes>
-          <Route path="/addTagPage"  component={AddTagPage} />;
-        </Routes>  */}
-        <AddTagPage></AddTagPage>
-        <MainPage></MainPage>
-    </div>
+
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/add" element={<AddTagPage />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
