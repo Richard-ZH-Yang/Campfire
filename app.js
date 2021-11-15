229579b561b0b1129a40f253af9adc004c854f0d
Richard <richardyangzh@gmail.com>
2021-11-14 17:25:25 -0800

    get random tag and get random content from by a tag

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '123456',
-    database : 'nodemysql'
+    password : '123456'
 });
 
 // Connect
@@ -19,11 +18,52 @@ db.connect((err) => {
 
 const app = express();
 
+
+// REQUIRES: tagName should be exact as the tag column in the database
+// EFFECTS: get a random content from a specific tag that is the same as the tagName. If the tagName does not exist,
+//          reject with NotFoundError
+app.get('/getRandomContent/:tag', (req, res) => {
+    let tagName = req.params.tag;
+
+    let sql = `SELECT DISTINCT content FROM folklore WHERE tag = '${tagName}'`;
+    let query = db.query(sql, (err, results) => {
+        if(err) {
+            res.status(400).json({result: "tag not found"});
+        }
+        if (results.length === 1 && results[0][tagName] === "") {
+            res.status(400).json({result: "There is no valid content for this tag"})
+        }
+        let randNum = getRandomIntInclusive(0, results.length);
+        res.status(200).json({result: results[randNum]})
+        // res.send('Posts fetched...');
+    });
+});
+
+// EFFECTS: get a random number between min and max inclusive
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+// EFFECTS: return a random tagName from the database. If the database is empty, reject with notFoundError
+app.get('/getRandomTag', (req, res) => {
+    let sql = `SELECT DISTINCT tag FROM folklore`;
+    let query = db.query(sql, (err, results) => {
+        if(err) {
+            res.status(400).json({result: "ERROR! Invalid query"});
+        }
+        let randNum = getRandomIntInclusive(0, results.length);
+        res.status(200).json({result: results[randNum]})
+        // res.send('Posts fetched...');
+    });
+});
+
 // Create DB
 app.get('/createdb', (req, res) => {
     let sql = 'CREATE DATABASE nodemysql';
     db.query(sql, (err, result) => {
-        if(err) throw err;
+        // if(err) throw err;
         console.log(result);
         res.send('Database created...');
     });
@@ -33,75 +73,12 @@ app.get('/createdb', (req, res) => {
 app.get('/createpoststable', (req, res) => {
     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
     db.query(sql, (err, result) => {
-        if(err) throw err;
+        // if(err) throw err;
         console.log(result);
         res.send('Posts table created...');
     });
 });
 
-// Insert post 1
-app.get('/addpost1', (req, res) => {
-    let post = {title:'Post One', body:'This is post number one'};
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql, post, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post 1 added...');
-    });
-});
-
-// Insert post 2
-app.get('/addpost2', (req, res) => {
-    let post = {title:'Post Two', body:'This is post number two'};
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql, post, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post 2 added...');
-    });
-});
-
-// Select posts
-app.get('/getposts', (req, res) => {
-    let sql = 'SELECT * FROM posts';
-    let query = db.query(sql, (err, results) => {
-        if(err) throw err;
-        console.log(results);
-        res.send('Posts fetched...');
-    });
-});
-
-// Select single post
-app.get('/getpost/:id', (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post fetched...');
-    });
-});
-
-// Update post
-app.get('/updatepost/:id', (req, res) => {
-    let newTitle = 'Updated Title';
-    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post updated...');
-    });
-});
-
-// Delete post
-app.get('/deletepost/:id', (req, res) => {
-    let newTitle = 'Updated Title';
-    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post deleted...');
-    });
-});
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
