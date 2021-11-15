c815aee964d8b37a26e0841465a5858ea2ede26d
Danielle Chen <daniellechen0317@gmail.com>
2021-11-14 18:45:36 -0800

    Search page

--- my-app/src/containers/SearchPage.js
+++ my-app/src/containers/SearchPage.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import {Link} from "react-router-dom";
 
@@ -19,7 +18,7 @@ function AddTagPage() {
         }
       }
 
-    function handleAdd(){
+    function handleSearch(){
         return
     }
 
@@ -30,24 +29,20 @@ function AddTagPage() {
   <div class="container col-xl-10 col-xxl-8 px-4 py-5">
     <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 mb-3" style={{width: 400, color:'white'}}>Add Your New Story Here</h1>
-        <p class="col-lg-10 fs-4" style={{color:'white'}}> Share your secret story to other people</p>
+        <h1 class="display-4 fw-bold lh-1 mb-3" style={{width: 400, color:'white'}}>Search the Tag You Want</h1>
+        <p class="col-lg-10 fs-4" style={{color:'white'}}> Read other people's story</p>
     </div>
       <div class="col-md-10 mx-auto col-lg-5">
         <form class="p-4 p-md-5 border rounded-3 bg-white" style={{width: 500, opacity: 0.8}}>
         <div class="form-floating mb-3">
-            <input class="form-control" id="tag" placeholder="text"/>
-            <label for="tag">Tag</label>
-          </div>
-          <div class="form-floating mb-3">
-            <textarea class="form-control" style={{height: 100}} id="newStory" placeholder="text"></textarea>
-            <label for="newStory">Your story</label>
+            <input class="form-control" id="searchTag" placeholder="text"/>
+            <label for="searchTag">Tag</label>
           </div>
           <div class="d-grid gap-2 d-md-flex justify-content-md-start">
             <Link to="/">
                 <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Back</button>
             </Link>
-            <button onClick={handleAdd} type="button" class="btn btn-outline-secondary btn-lg px-4">Submit</button>
+            <button onClick={handleSearch} type="button" class="btn btn-outline-secondary btn-lg px-4">Search</button>
         </div>
           <hr class="my-4"/>
         </form>
