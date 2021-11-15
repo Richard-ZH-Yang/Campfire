e925886e69b02382828f620ddc88a2b42fe6cad8
Danielle Chen <daniellechen0317@gmail.com>
2021-11-14 18:08:01 -0800

    finalize add tag page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,11 @@ import { BrowserRouter as Routes ,Route } from 'react-router-dom';
 import './App.css';
 
 import MainPage from "./containers/MainPage";
-import AddTagPage from "./containers/addTagPage";
+import AddTagPage from "./containers/AddTagPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-{/*   
-        <Routes>
-          <Route path="/addTagPage"  component={AddTagPage} />;
-        </Routes>  */}
         <AddTagPage></AddTagPage>
         <MainPage></MainPage>
     </div>
