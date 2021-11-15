159d2a9e528923a5d11d96aa82c43263715b890e
mading11 <mading11@students.cs.ubc.ca>
2021-11-14 17:56:04 -0800

    Add folkflore and search tag implementation finished.

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/createfolkloretable', (req, res) => {
 });
 
 // Insert post 1
-app.get('/addfolkflore/:tag/:content', (req, res) => {
+app.get('/addfolkflore/:tag/:content?', (req, res) => {
     // let tag = ${req.params.tag};
     // let content = req.value.get('content');
     let uid = uuid.v4();
@@ -70,37 +70,49 @@ app.get('/searchtag/:tag', (req, res) => {
     });
 });
 
-// Select single post
-app.get('/getrandomcontent', (req, res) => {
-    let sql = `SELECT DISTINCT content FROM folklore WHERE tag = ${req.params.tag}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post fetched...');
+// REQUIRES: tagName should be exact as the tag column in the database
+// EFFECTS: get a random content from a specific tag that is the same as the tagName. If the tagName does not exist,
+//          reject with NotFoundError
+app.get('/getRandomContent/:tag', (req, res) => {
+    let tagName = req.params.tag;
+
+    let sql = `SELECT DISTINCT content FROM folklore WHERE tag = '${tagName}'`;
+    let query = db.query(sql, (err, results) => {
+        if(err) {
+            res.status(400).json({result: "tag not found"});
+        }
+
+        if (results.length === 1 && results[0][tagName] === undefined) {
+            res.status(400).json({result: "There is no valid content for this tag"})
+        }
+        let randNum = getRandomIntInclusive(0, results.length-1);
+        res.status(200).json({result: results[randNum]})
+        // res.send('Posts fetched...');
     });
 });
 
-// Update post
-app.get('/updatepost/:id', (req, res) => {
-    let newTitle = 'Updated Title';
-    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post updated...');
+// EFFECTS: return a random tagName from the database. If the database is empty, reject with notFoundError
+app.get('/getRandomTag', (req, res) => {
+    let sql = `SELECT DISTINCT tag FROM folklore`;
+    let query = db.query(sql, (err, results) => {
+        if(err) {
+            res.status(400).json({result: "ERROR! Invalid query"});
+        }
+        if (results.length ===  0) {
+            res.status(400).json({result: "Dataset is empty"})
+        }
+        let randNum = getRandomIntInclusive(0, results.length-1);
+        res.status(200).json({result: results[randNum]})
+        // res.send('Posts fetched...');
     });
 });
 
-// Delete post
-app.get('/deletepost/:id', (req, res) => {
-    let newTitle = 'Updated Title';
-    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post deleted...');
-    });
-});
+// EFFECTS: get a random number between min and max inclusive
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
